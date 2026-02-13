7b90b6b62c499b85dfc504361dff8805f570db3f
OM DHAGE <omdhage.dev@gmail.com>
2026-02-13 19:12:52 +0000

    added hoter server address

--- app.py
+++ app.py
@@ -390,4 +390,5 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
